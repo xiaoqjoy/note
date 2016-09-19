8c20989b53ff463dbff54fe5f6e7aa05e4139744
xiaoqjoy <dream_w@yeah.net>
2016-09-19 18:13:40 +0800

    pp

---
+++ javaDemo/study.java
@@ -0,0 +1,10 @@
+class dStudy
+{
+	public static void main(String[] args)
+	{
+		int x = 3,y;
+		
+		y = (x>1)?100:300;
+		System.out.println(y);
+	}
+}
---
+++ javaDemo/switch.java
@@ -0,0 +1,61 @@
+class Switch
+{
+	public static void main(String[] args)
+	{
+		xiaoqiang:for(int x=0; x<3; x++)
+		{
+			wangcai:for(int y=0; y<4; y++)
+			{
+				System.out.println("x="+x);
+				break;
+			}
+		}
+		
+		
+		
+		
+		/*int x = 2;
+		switch(x)
+		{
+			default:
+			System.out.println("d");
+			//break;
+			case 1:
+			System.out.println("a");
+			//break;
+			case 2:
+			System.out.println("b");
+			break;
+			case 3:
+			System.out.println("e");
+			break;
+		};
+		
+		for(int y=1; y<5; y++)
+		{
+			System.out.println(y);
+		};
+		//System.out.println("y=="+y);
+		
+		for(int a=0; a<3; a++)
+		{
+			for(int b=0; b<4; b++)
+			{
+				System.out.println("ok!");
+			}
+		};
+		
+		for(int c=0; c<4; c++)
+		{
+			for(int d=0; d<5; d++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		*/
+		
+		
+		
+	}
+}
