995c5f76fa5434cae47ffc26556eaa113cf858d3
wuhaiwei <dream_w@yeah.net>
2019-03-20 17:43:37 +0800

    git用法

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -3,218 +3,250 @@ import logo from './logo.svg';
 import './App.css';
 
 function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+  return user.firstName + ' ' + user.lastName;
 }
 
 const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
+  firstName: 'Harper',
+  lastName: 'Perez'
 };
 
 const element = (
-    <div>
-        <h1>Hello!111111111</h1>
-        <h2>Good to see you here.</h2>
-    </div>
+  <div>
+    <h1>Hello!111111111</h1>
+    <h2>Good to see you here.</h2>
+  </div>
 );
 
 const aElement = React.createElement(
-    'h1',
-    {className: 'greeting'},
-    'Hello, world!'
+  'h1',
+  { className: 'greeting' },
+  'Hello, world!'
 );
 
 function Welcome(props) {                      //组件名称必须以大写字母开头
-    return <h1>Hello, {props.name}</h1>;
+  return <h1>Hello, {props.name}</h1>;
 }
 
 function formatDate(date) {
-    return date.toLocaleDateString();
+  return date.toLocaleDateString();
 }
 
 function Avatar(props) {
-    return (
-        <img className="Avatar" title={props.user.name} src={props.user.avatarUrl} alt={props.user.name}/>
-    );
+  return (
+    <img className="Avatar" title={props.user.name} src={props.user.avatarUrl} alt={props.user.name} />
+  );
 }
 
 function UserInfo(props) {    //提取组件
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
-            </div>
-        </div>
-    );
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
 }
 
 function Comment(props) {
-    return (
-        <div className="Comment">
-            {/*<div className="UserInfo">
+  return (
+    <div className="Comment">
+      {/*<div className="UserInfo">
                 <Avatar user={props.author} />
                 <div className="UserInfo-name">{props.author.name}</div>
             </div>*/}
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
 }
 
 const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-    }
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
 };
 
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-function handleClick(e){
-    e.preventDefault();
-    console.log("click");
+function handleClick(e) {
+  e.preventDefault();
+  console.log("click");
 
-    /*const Person = {
-        'name': 'little bear',
-        'age': 18,
-        'sayHello': function () {          //对象的括号是不能封闭作用域的。所以此时的this还是指向全局对象。
-            console.log(this);
-            console.log('我叫' + this.name + '我今年' + this.age + '岁!')
-            return function(){
-                console.log(this);
-                //console.log('我叫' + this.name + '我今年' + this.age + '岁!')
-            }
+  /*const Person = {
+      'name': 'little bear',
+      'age': 18,
+      'sayHello': function () {          //对象的括号是不能封闭作用域的。所以此时的this还是指向全局对象。
+          console.log(this);
+          console.log('我叫' + this.name + '我今年' + this.age + '岁!')
+          return function(){
+              console.log(this);
+              //console.log('我叫' + this.name + '我今年' + this.age + '岁!')
+          }
 
-        }
-    }
-    //Person.sayHello();
-    Person.sayHello()();*/
+      }
+  }
+  //Person.sayHello();
+  Person.sayHello()();*/
 
-    /*function Person () {
-        this.name = 'little bear';
-        this.age = 18;
-        let self = this;
-        setInterval(function sayHello () {
-            console.log('我叫' + self.name + '我今年' + self.age + '岁!')
-        }, 1000)
-    }
-    let p = new Person()*/
+  /*function Person () {
+      this.name = 'little bear';
+      this.age = 18;
+      let self = this;
+      setInterval(function sayHello () {
+          console.log('我叫' + self.name + '我今年' + self.age + '岁!')
+      }, 1000)
+  }
+  let p = new Person()*/
 
-    function Person () {
-        this.name = 'little bear';
-        this.age = 18;
+  function Person() {
+    this.name = 'little bear';
+    this.age = 18;
+    console.log('我叫' + this.name + '我今年' + this.age + '岁')
+    /*setInterval(() => {
         console.log('我叫' + this.name + '我今年' + this.age + '岁')
-        /*setInterval(() => {
-            console.log('我叫' + this.name + '我今年' + this.age + '岁')
-        },1000)*/
-    }
-    let p = new Person()
+    },1000)*/
+  }
+  let p = new Person()
+  return p;
 
 }
 
 class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isToggleOn: true};
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
 
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-    }
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
-    }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>
-                    {this.state.isToggleOn ? 'ON' : 'OFF'}
-                </button>
-                <p>22222222222222222222222</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+        <p>22222222222222222222222</p>
+      </div>
+    );
+  }
 }
 
 class ShoppingList extends React.Component {
-    render() {
-        return (
-        <div className="shopping-list">
-            <h1>Shopping List for</h1>
-            <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-            </ul>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+        <ul>
+          <li>33333</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 let pic = {
-    uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+  url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
 };
+
+class UserComponent extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      value: '6666666'
+    }
+  }
+  getUser(){
+    console.log("neo")
+  }
+  renderData(){
+    return <span>iiiiiiiiiiiiiiiiiiiiii</span>
+  }
+  render(){
+    return (
+      <div className="user">
+        neo<img src={pic.url} title="香蕉" alt="user" />
+        <input type="button" value="点击" onClick={this.getUser} />
+        <button onClick={() => console.log('button')}>1111click</button>
+        <p>{this.state.value}</p>
+        <p>{this.renderData()}</p>
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {    //App组件类
   render() {
     return (
       <div className="App">
-        <img src={pic.uri} style={{width: 193, height: 110}} />
+        <UserComponent></UserComponent>
+        <img src={pic.url} alt="" style={{ width: 193, height: 110 }} />
         <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-		  <span>2222222222222222222222</span>
+          <span>2222222222222222222222</span>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Hello, {formatName(user)}!</h1>
-        { element } { aElement }
+        {element} {aElement}
         <Clock />
         <Toggle />
         <ShoppingList></ShoppingList>
-        <a href="#" className="click" onClick={handleClick}>click</a>
+        <a href="" className="click" onClick={handleClick}>click222222333333</a>
         <Welcome name="Sara" />
         <Welcome name="Cahal" />
         <Welcome name="Edite" />
--- react.md
+++ react.md
@@ -44,7 +44,9 @@ npm start
 HTML 里的 class 在 JSX 里要写成 className，因为 class 在 JS 里是保留关键字
 同理某些属性比如 for 要写成 htmlFor
 
-React 在这个 Virtual DOM 上实现了一个 diff 算法，当要重新渲染组件的时候，会通过 diff 寻找到要变更的 DOM 节点，再把这个修改更新到浏览器实际的 DOM 节点上，所以实际上不是真的渲染整个 DOM 树
+React 在这个 Virtual DOM 上实现了一个 diff 算法，当要重新渲染组件的时候，会通过 diff 寻找到要变更的 DOM 节点，
+再把这个修改更新到浏览器实际的 DOM 节点上，所以实际上不是真的渲染整个 DOM 树
+
 
 
 注释 在一个组件的子元素位置使用注释要用 {} 包起来
@@ -56,14 +58,25 @@ React 在这个 Virtual DOM 上实现了一个 diff 算法，当要重新渲染�
 
 constructor( )——构造方法
 
-       这是ES6对类的默认方法，通过 new 命令生成对象实例时自动调用该方法。并且，该方法是类中必须有的，如果没有显示定义，则会默认添加空的constructor( )方法。
+       这是ES6对类的默认方法，通过 new 命令生成对象实例时自动调用该方法。并且，该方法是类中必须有的，
+如果没有显示定义，则会默认添加空的constructor( )方法。
 
 
 super( ) ——继承
 
-    在class方法中，继承是使用 extends 关键字来实现的。子类 必须 在 constructor( )调用 super( )方法，否则新建实例时会报错。
+    在class方法中，继承是使用 extends 关键字来实现的。子类 必须 在 constructor( )调用 super( )方法，
+	否则新建实例时会报错。
 
-    报错的原因是：子类是没有自己的 this 对象的，它只能继承自父类的 this 对象，然后对其进行加工，而super( )就是将父类中的this对象继承给子类的。没有 super，子类就得不到 this 对象。
+    报错的原因是：子类是没有自己的 this 对象的，它只能继承自父类的 this 对象，然后对其进行加工，
+而super( )就是将父类中的this对象继承给子类的。没有 super，子类就得不到 this 对象。
+
+
+
+React 使用 JSX 来替代常规的 JavaScript
+
+JSX 执行更快，因为它在编译为 JavaScript 代码后进行了优化。
+它是类型安全的，在编译过程中就能发现错误。
+使用 JSX 编写模板更加简单快速。
 
 
 
