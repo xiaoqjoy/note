95a1f2771bce43bf7c46939c4a3f7c22d0c37c70
xiaoqijoy <dream_w@yeah.net>
2020-09-27 12:54:29 +0800

    ++

--- Vue-demo2/src/home/test.vue
+++ Vue-demo2/src/home/test.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div id="app">
     <p>this is test file!</p>
     <span>{{ name }}--------------------test page</span>
     <span>{{ count }}-------------------test page</span>
     <p>{{ num }} {{ searchQuery }} {{ gridColumns[0] }}-----{{ gridColumns[1] }}</p>
+    <p>{{ msg }}</p>
   </div>
 </template>
 
@@ -12,15 +13,22 @@
       data(){
         return{
           name: this.$store.state.name,
-          count: this.$store.state.count
+          count: this.$store.state.count,
+          msg: this.for
         }
       },
+      inject: ['for'],
       props: ['searchQuery','gridColumns','gridData'],
       name: "test",
       computed: {
         num(){
           return this.gridColumns[1]
         }
+      },
+      created(){
+        console.log(this.$root)
+        console.log(this.$el)
+        console.log(this.$data)
       },
       mounted(){
         console.log(this.$store.state.name);
