91bc03c3b78be00c99ba0986364760836c2f0d83
xiaoqijoy <dream_w@yeah.net>
2021-04-25 17:06:59 +0800

    插槽的使用

--- vue-demo/src/home/test.vue
+++ vue-demo/src/home/test.vue
@@ -2,50 +2,50 @@
   <div>
     <p>this is test file!</p>
     <span>{{ name }}--------------------test page</span>
-    <span  ref="dom">{{ count }}-------------------test page</span>
+    <span ref="dom">{{ count }}-------------------test page</span>
     <p>{{ num }}</p>
     <p>{{ parm }}</p>
     <p>----------------------------------------------++++++++++++++++++++++++++</p>
+    <slot></slot>
 
   </div>
 </template>
 
 <script>
-    export default {
-      name: "test",
-      props: ['parm'],
-      data(){
-        return{
-          name: this.$store.state.name,
-          count: this.$store.state.count,
-          a: 123,
-          b: 456,
-          firstName: 'Foo',
-          lastName: 'Bar'
-        }
-      },
-      
-      created(){
-        console.log(this.$store)
-        //setTimeout('console.log(this.$refs.dom)',0)
-        this.$nextTick(() => {
-          console.log(this.$refs.dom)
-        })
-      },
-      computed: {
-        num(){
-          return this.a + this.b
-        }
-      },
-      watch: {
-        
-      },
-      mounted(){
-        console.log(this.$refs.dom)
-      }
+export default {
+  name: "test",
+  props: ['parm'],
+  data() {
+    return {
+      name: this.$store.state.name,
+      count: this.$store.state.count,
+      a: 123,
+      b: 456,
+      firstName: 'Foo',
+      lastName: 'Bar'
     }
+  },
+
+  created() {
+    console.log(this.$store)
+    //setTimeout('console.log(this.$refs.dom)',0)
+    this.$nextTick(() => {
+      console.log(this.$refs.dom)
+    })
+  },
+  computed: {
+    num() {
+      return this.a + this.b
+    }
+  },
+  watch: {
+
+  },
+  mounted() {
+    console.log(this.$refs.dom)
+  }
+}
 </script>
 
 <style scoped>
-
 </style>
