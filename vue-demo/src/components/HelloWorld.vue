61f26087c8ac489f77bf3ecf9ee92e4f86132e09
xiaoqjoy <dream_w@yeah.net>
2021-05-20 15:22:34 +0800

    add 滚动内存拉爆问题

--- vue-demo/src/components/HelloWorld.vue
+++ vue-demo/src/components/HelloWorld.vue
@@ -1,68 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-      <br>
-      <li>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
+  <div>
+    <p>vue针对滚动元素内部大量元素，但只有部分元素可见，对dom懒渲染，节省内存的优化</p>
+    <div class="list" style="height: 300px;overflow: scroll" ref="scrollDom" @scroll="scroll">
+      <div :style="{height:list.length*40+'px'}"></div>
+      <div style="position:absolute;width: 100%" :style="{top:startIndex*40+'px'}">
+        <div v-for="(item,index) in splitData" :key="index" class="item">
+          <div v-html="item.id"></div>
+          <div v-html="item.name"></div>
+          <div v-html="item.createTime"></div>
+          <div>
+            <Button>修改</Button>
+            <Button>删除</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  created() {
+    for (let i = 0; i < 10000; i++) {
+      this.list.push({
+        id: 1,
+        name: 'name' + i,
+        createTime: '2018-09-09'
+      })
+    }
+  },
+  computed: {
+    limitCount() {
+      return 300 / 40 + 2;
+    },
+    splitData() {
+      return this.list.slice(this.startIndex, this.startIndex + this.limitCount)
+    }
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      startIndex: 0,
+      list: []
+    }
+  },
+  methods: {
+    scroll() {
+      // 根据滚动的距离，估算出这个滚动位置对应的数组序列，例如滚动100px，每条40px，对应第3条
+      let scrollTop = this.$refs.scrollDom.scrollTop;
+      this.startIndex = Math.floor(scrollTop / 40);
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.list {
+  border: solid 1px #5f5f5f;
+  background-color: white;
+  margin: 100px 0;
+  padding: 5px;
+  width: 500px;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.item {
+  display: flex;
+  height: 40px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.item div {
+  flex-grow: 1;
+  border: solid 1px #9e9e9e;
+  padding: 3px;
 }
 </style>
